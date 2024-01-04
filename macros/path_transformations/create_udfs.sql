fd05e417e7f61b50de1b1700557d38fcab0eadc8
Agnes Kiss <agnes@snowplowanalytics.com>
2024-01-04 18:15:12 +0000

    Change varchar in udfs

--- macros/path_transformations/create_udfs.sql
+++ macros/path_transformations/create_udfs.sql
@@ -5,14 +5,12 @@ and you may not use this file except in compliance with the Snowplow Personal an
 You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 at https://docs.snowplow.io/personal-and-academic-license-1.0/
 #}
 
-
 {% macro create_udfs() %}
   {{ return(adapter.dispatch('create_udfs', 'snowplow_attribution')()) }}
 {% endmacro %}
 
 {% macro default__create_udfs() %}
 {% endmacro %}
-
 
 {% macro bigquery__create_udfs() %}
 
@@ -189,7 +187,7 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
   -- Returns the last snowplow__path_lookback_steps channels in the path if snowplow__path_lookback_steps > 0,
   -- or the full path otherwise.
   create or replace function {{target.schema}}.trim_long_path(path varchar(max), snowplow__path_lookback_steps integer)
-  returns varchar
+  returns varchar(max)
   stable
   AS $$
   path_list = path.split(' > ')
@@ -220,7 +218,7 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
   -- Returns the path with all copies of targetElem removed, unless the path consists only of
   -- targetElems, in which case the original path is returned.
   create or replace function {{target.schema}}.remove_if_not_all(path varchar(max), target_elem varchar)
-  returns varchar
+  returns varchar(max)
   stable
   AS $$
 
@@ -246,7 +244,7 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
   -- Returns the path with all copies of targetElem removed from the tail, unless the path consists
   -- only of targetElems, in which case the original path is returned.
   create or replace function {{target.schema}}.remove_if_last_and_not_all(path varchar(max), target_elem varchar)
-  returns varchar
+  returns varchar(max)
   stable
   AS $$
 
@@ -277,7 +275,7 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
   -- Returns the unique/identity transform of the given path array.
   -- E.g. [D, A, B, B, C, D, C, C] --> [D, A, B, B, C, D, C, C].
   create or replace function {{target.schema}}.unique_path(path varchar(max))
-  returns varchar
+  returns varchar(max)
   stable
   AS $$
 
@@ -292,7 +290,7 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
   -- E.g. [D, A, B, B, C, D, C, C] --> [D, A, B, C, D, C].
 
   create or replace function {{target.schema}}.exposure_path(path varchar(max))
-  returns varchar
+  returns varchar(max)
   stable
   AS $$
 
@@ -316,7 +314,7 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
   -- Repeated channels are removed.
   -- E.g. [D, A, B, B, C, D, C, C] --> [D, A, B, C].
   create or replace function {{target.schema}}.first_path(path varchar(max))
-  returns varchar
+  returns varchar(max)
   stable
   AS $$
 
@@ -339,7 +337,7 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
   -- Repeat events are removed, but tracked with a count.
   -- E.g. [D, A, B, B, C, D, C, C] --> [D(2), A(1), B(2), C(3)].
   create or replace function {{target.schema}}.frequency_path(path varchar(max))
-  returns varchar
+  returns varchar(max)
   stable
   AS $$
 
