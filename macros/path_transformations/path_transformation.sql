cd3ea26fe41a8c073978b53485be506ef8f84974
Agnes Kiss <agnes@snowplowanalytics.com>
2026-03-09 14:42:15 +0000

    Avoid using unnecessary macros

--- macros/path_transformations/path_transformation.sql
+++ macros/path_transformations/path_transformation.sql
@@ -23,6 +23,11 @@ You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 
 {% endmacro %}
 
 {% macro redshift__path_transformation(transformation_type, transform_param, field_alias) %}
+
+    {{ exceptions.raise_compiler_error(
+      "Snowplow Error: Path transformations on Redshift are handled via redshift__transform_paths / LISTAGG in the main model SQL and not via this macro. 
+      For unit testing please use the dedicated test in the integration testing module."
+      ) }}
 {% endmacro %}
 
 {% macro spark__path_transformation(transformation_type, transform_param, field_alias) %}
